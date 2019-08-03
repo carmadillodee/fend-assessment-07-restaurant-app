940791e458821680f2475fc395c495638a289544
carmadillodee <48983161+carmadillodee@users.noreply.github.com>
2019-08-04 03:18:02 +1000

    Added descriptions + attributions - cleaned up code.
    Addedd code notes + beautified files for neater code

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-console.log('SW registered again!');
- const cacheCurrent = 'rest-v1';
+//console.log('SW registered again!');
 
-//items to be cached
+
+const cacheCurrent = 'rest-v1';
+
+//items to be cached - lost awhole day because I forgot that semi-colon!!!
 const cacheFiles = [
   '/',
   '/index.html',
@@ -26,48 +28,40 @@ const cacheFiles = [
 //add files to cache on install
 self.addEventListener('install', function(event) {
 
-event.waitUntil(
-  caches.open('cacheCurrent').then(function(cache) {
-    console.log(cache);
-    return cache.addAll(cacheFiles);
+  event.waitUntil(
+    caches.open('cacheCurrent').then(function(cache) {
+      console.log(cache);
+      return cache.addAll(cacheFiles);
 
-  }).catch(error => {
-    console.log(error);
-  })
-);
+    }).catch(error => {
+      console.log(error);
+    })
+  );
 });
 
-//check if files are in the cache and fetch them if not.
+//check if files are in the cache and fetch them if not. Alert if error.
+//This code was modified from instructions I stepped through in Matthew Cranford's walkthrough:
+// https://matthewcranford.com/restaurant-reviews-app-walkthrough-part-4-service-workers/
 self.addEventListener('fetch', function(event) {
   console.log('fetching...');
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      if(response){
+      if (response) {
         console.log('got response');
         return response
       } else {
         console.log('getting request and adding to cache...');
         return fetch(event.request)
-        .then(function(response) {
-          const responseClone = response.clone();
-          caches.open('rest-v1').then(function(cache) {
-            cache.put(event.request, responseClone);
-          })
-          return response;
+          .then(function(response) {
+            const responseClone = response.clone();
+            caches.open('rest-v1').then(function(cache) {
+              cache.put(event.request, responseClone);
+            })
+            return response;
           })
           .catch(function(err) {
             console.log(err);
+          });
+      }
+    }));
 });
-}
-}));
-});
-
-/*
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-*/
